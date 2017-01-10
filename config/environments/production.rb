d7259249df0e46c059fbee49311faa0a549264ca
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-01-10 14:28:37 +0800

    Change domain for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'calm-mesa-56372.herokuapp.com',
+    :domain => 'cynergy-ors.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
