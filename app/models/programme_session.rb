e81f5f30b771dc4f47dd0138364b9a34ae5eec23
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-01-11 14:59:52 +0800

    Add scope :active to ProgrammeSession

--- app/models/programme_session.rb
+++ app/models/programme_session.rb
@@ -2,4 +2,6 @@ class ProgrammeSession < ActiveRecord::Base
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  scope :active, -> { where(active: true) }
 end
