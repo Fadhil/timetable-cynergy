2f8fc955873c8287df844ae62f2eb17430e51fee
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-01-17 16:31:54 +0800

    Add listing_name for ProgrammeModule

--- app/models/programme_module.rb
+++ app/models/programme_module.rb
@@ -2,4 +2,8 @@ class ProgrammeModule < ActiveRecord::Base
   has_and_belongs_to_many :sessions, class_name: 'ProgrammeSession'
   belongs_to :category
   has_and_belongs_to_many :registrations
+
+  def listing_name
+    "#{code}: #{name}"
+  end
 end
