e7808a014e8d045e703a0ebfcb9ba91bd6ca9979
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-01-17 15:30:59 +0800

    Update DB seed
    * ProgrammeModules and Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,42 @@
 
 roles_list = ["Admin"]
 
+puts "Creating roles\n"
 roles_list.each do |role|
   Role.where(name: role).first_or_create
+end
+module_categories_list = [["COMMUNICATION", "COMM"],
+                          ["INFORMATION & COMMUNICATION TECHNOLOGY", "ICT"],
+                          ["MANAGING SELF AT WORKPLACE","MSW"],
+                          ["MOTIVATIONAL","MOTIV"],
+                          ["HUMAN DEVELOPMENT","HUMDEV"]]
+
+puts "Creating Categories\n"
+
+module_categories_list.each do |category, code|
+  Category.where(name: category, code: code).first_or_create
+end
+
+modules_list = [
+  ["COMM","1-A", "Small Steps, Big Results in Reading Critically"],
+  ["COMM","1-B", "Write Right - Winning Ways with Writing"],
+  ["COMM","1-C", "Small Steps, Big Results : Heart & Soul of Communication"],
+  ["ICT","2-A", "Professional Workplace Application"],
+  ["ICT","2-B", "Multimedia Applications fat Workplace"],
+  ["MSW","3-A", "Managing Change at Workplace"],
+  ["MSW","3-B", "Winnable Relationship"],
+  ["MOTIV","4-A", "Leader in You"],
+  ["MOTIV","4-B", "Qiamulail Programme and Jatidiri Session"],
+  ["HUMDEV","5-A", "Personal Effectiveness Toolkit"],
+  ["HUMDEV","5-B", "Grooming & Etiquettes for Young Professionals"]
+]
+
+puts "Creating modules \n"
+modules_list.each do |category_code, module_code, module_name|
+  programme_module = ProgrammeModule.where(code: module_code, name: module_name).first_or_create
+  if programme_module
+    category = Category.where(code: category_code).first
+    programme_module.category =  category unless category.nil?
+    programme_module.save
+  end
 end
