2d23b1452107f18928340f75c113a8c688acf7eb
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-01-17 23:15:58 +0800

    Can highlight timetable

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  var table = document.getElementsByTagName("table")[0];
+  var cells = table.getElementsByTagName("td"); // 
+
+  for(var i = 0; i < cells.length; i++){
+      // Cell Object
+      var cell = cells[i];
+      // Track with onclick
+      cell.onclick = function(){
+          toggleSelect(this);
+          //alert("cell: " + cellIndex + " / row: " + rowIndex );
+      }
+  }
+});
+
+function toggleSelect(cell) {
+  var cellIndex  = cell.cellIndex;  
+
+  var rowIndex = cell.parentNode.rowIndex;
+ 
+  if(rowIndex!==0 && cellIndex !== 0) {
+    if($(cell).hasClass("highlighted")){
+      unHighlighCell(cell);
+    } else {
+      highlightCell(cell);
+    }
+  }
+}
+
+function highlightCell(cell) {
+  $(cell).addClass("highlighted");
+}
+
+function unHighlighCell(cell) {
+  $(cell).removeClass("highlighted");
+}
