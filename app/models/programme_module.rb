d2de2aab536a8a6ccefa68f10aed9d489cf0ff69
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-01-17 15:39:36 +0800

    ProgrammeModules<HABTM>Registrations

--- app/models/programme_module.rb
+++ app/models/programme_module.rb
@@ -1,4 +1,5 @@
 class ProgrammeModule < ActiveRecord::Base
   has_and_belongs_to_many :sessions, class_name: 'ProgrammeSession'
   belongs_to :category
+  has_and_belongs_to_many :registrations
 end
